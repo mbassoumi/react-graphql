86a7b3d07087063682b53800adbdfce015bc36f4
majdbassoumi <mbbassoumi@gmail.com>
2019-12-03 23:45:22 +0200

    work with client!

--- src/components/ExchangeRates.tsx
+++ src/components/ExchangeRates.tsx
@@ -12,6 +12,15 @@ const EXCHANGE_RATES = gql`
     }
 `;
 
+const AUTHOR_QUERY = gql`
+    query Authers($id: ID!) {
+        author(id: $id) {
+            firstName @client
+        }
+    }
+`;
+
+
 interface MyProps {
     currency: string,
     rate: number,
@@ -19,33 +28,44 @@ interface MyProps {
 
 const ExchangeRates = () => {
     // const {loading, error, data} = useQuery(EXCHANGE_RATES);
-    const {loading, error, data, refetch, networkStatus} = useQuery(EXCHANGE_RATES, {
+    // const {loading, error, data, refetch, networkStatus} = useQuery(EXCHANGE_RATES, {
+    //     variables: {
+    //         currency: 'USD'
+    //     },
+    //     skip: !'USD',
+    //     // pollInterval: 500,
+    //     notifyOnNetworkStatusChange: true
+    // });
+    //
+    const {loading, error, data, refetch, networkStatus} = useQuery(AUTHOR_QUERY, {
         variables: {
-            currency: 'USD'
+            id: 1
         },
-        skip: !'USD',
         // pollInterval: 500,
         notifyOnNetworkStatusChange: true
     });
+
+
 
     if (networkStatus === 4) return <p>Refetching ...</p>;
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :( {error.message}</p>;
 
+    console.log(data);
     return (
         <div>
             <button onClick={() => refetch()}>Refetch</button>
-            {
-                data.rates.map(({currency, rate}: MyProps) => (
-                    <div key={currency}>
-                        <p>
-                            {currency}: {rate}
-                        </p>
-                    </div>
-                ))
-            }
+            {/*{*/}
+            {/*    data.rates.map(({currency, rate}: MyProps) => (*/}
+            {/*        <div key={currency}>*/}
+            {/*            <p>*/}
+            {/*                {currency}: {rate}*/}
+            {/*            </p>*/}
+            {/*        </div>*/}
+            {/*    ))*/}
+            {/*}*/}
         </div>
-    )
+    );
 };
 
 export default ExchangeRates;
--- src/graphql/client.tsx
+++ src/graphql/client.tsx
@@ -1,5 +1,66 @@
 import ApolloClient from 'apollo-boost';
+import {gql, InMemoryCache, HttpLink} from 'apollo-boost';
+
+const typeDefs = gql`
+    type Author {
+        id: Int!
+        firstName: String!
+        lastName: String!
+#        """
+#        the list of Posts by this author
+#        """
+#        posts: [Post]
+    }
+    
+    type Query {
+        author(id: ID) : Author
+    }
+
+#    type Post {
+#        id: Int!
+#        title: String
+#        author: Author
+#        votes: Int
+#    }
+#
+#    # the schema allows the following query:
+#    type Query {
+#        posts: [Post]
+#        author(id: Int!): Author
+#    }
+#
+#    # this schema allows the following mutation:
+#    type Mutation {
+#        upvotePost (
+#            postId: Int!
+#        ): Post
+#    }
+`;
+
+const resolvers = {
+    Author: {
+        id: () => 1,
+        firstName: () => {
+            console.log('fuckkkk');
+            return 'Majd'
+        },
+        lastName: () => 'Basem'
+    }
+};
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: 'https://48p1r2roz4.sse.codesandbox.io'
+});
 
 export const client = new ApolloClient({
+    cache,
     uri: 'https://48p1r2roz4.sse.codesandbox.io',
+    typeDefs,
+    resolvers
 });
+
+
+// export const client = new ApolloClient({
+//     uri: 'https://48p1r2roz4.sse.codesandbox.io',
+// });
